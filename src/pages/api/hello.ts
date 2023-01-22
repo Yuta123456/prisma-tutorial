45039f058173e11f80ed4fab87fabe18b56f8e11
Yuta123456 <yuuta09090530@icloud.com>
2023-01-22 13:55:29 +0900

    init prisma + next.js

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, User } from "@prisma/client";
 
 type Data = {
-  name: string
-}
+  users: User[];
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const prisma = new PrismaClient();
+  const requestHandler = async () => {
+    const users = await prisma.user.findMany();
+    res.status(200).json({ users: users });
+  };
+  requestHandler()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
 }
